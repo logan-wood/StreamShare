3d82b5ba156470c182282979187034f9c1947398
logan-wood <63706140+logan-wood@users.noreply.github.com>
2024-07-26 16:18:13 +0900

    Initial Commit

---
+++ StreamShare/StreamShareApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreamShareApp.swift
+//  StreamShare
+//
+//  Created by Logan Wood on 26/07/24.
+//
+
+import SwiftUI
+
+@main
+struct StreamShareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
